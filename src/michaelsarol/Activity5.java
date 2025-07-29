822f1973a45c56e6d8e21e209b8dd60bb7fc5c5b
michaelsarol <michaelsarol143@gmail.com>
2025-07-28 23:44:17 -0700

    Assignment

--- src/michaelsarol/Activity5.java
+++ src/michaelsarol/Activity5.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package michaelsarol;
 
-/**
- *
- * @author PC 16
- */
 public class Activity5 {
+    public static void main(String[] args) {
+        System.out.println("Activity2");
+    }
     
 }
